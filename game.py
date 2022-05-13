647638130bc33704ca5846c74dcf5debacf95c9f
Andres <andres-177@hotmail.es>
2022-05-12 21:23:20 -0400

    warship

--- game.py
+++ game.py
@@ -8,11 +8,12 @@ from enemy import Enemy
 class Warship:
     def __init__(self):
         pygame.init()#inicializa el modulo
-        self.width = 1000
-        self.surface = pygame.display.set_mode((self.width, HEIGHT))#surface crea la ventana del juego
+        self.width = 800
+        self.height = 500
+        self.surface = pygame.display.set_mode((self.width, self.height))#surface crea la ventana del juego
         self.surface_width = self.surface.get_rect().width
         self.surface_height = self.surface.get_rect().height
-        self.background = pygame.transform.scale(pygame.image.load(os.path.join('image/universe.jpg')),(self.width, HEIGHT))#agrega el background del juego
+        self.background = pygame.transform.scale(pygame.image.load(os.path.join('image/universe.jpg')),(self.width, self.height))#agrega el background del juego
         pygame.display.set_caption(TITLE)
         self.surface.fill(BLUE)
         self.speed = 1
@@ -69,16 +70,27 @@ class Warship:
     
     def _create_fleet(self):
         enemy = Enemy(self)
-        enemy_width = enemy.rect.width
-        #self.enemies.add(enemy)
+        enemy_width, enemy_height = enemy.rect.size
         availableSpace = self.width - (2 * enemy_width)
         numerodeEnemies = availableSpace // ( 2 * enemy_width)
+        spaceship_height = self.spaceship.rect.height
+        availableSpacey = self.height - (3 * enemy_height) - spaceship_height
+        numerodeFilas = availableSpacey // (2 * enemy_height)
+        
+        for fila in range (numerodeFilas):
+            for numeroEnemy in range(numerodeEnemies):
+                self.__create__enemy(numeroEnemy, fila)
 
-        for numeroenemy in range(numerodeEnemies):
-            enemy = Enemy(self)
-            enemy.x = enemy_width + 2* enemy_width * numeroenemy
-            enemy.rect.x = enemy.x
-            self.enemies.add(enemy)
+
+    def __create__enemy(self, numeroEnemy, fila):
+        enemy = Enemy(self)
+        enemy_width, alien_height = enemy.rect.size
+        enemy.x = enemy_width + 2* enemy_width * numeroEnemy
+        enemy.rect.x = enemy.x
+        enemy.rect.y = enemy.rect.height + 2*enemy.rect.height * fila
+        self.enemies.add(enemy)
+                    
+                
 
 if __name__ == '__main__':
     w = Warship()
